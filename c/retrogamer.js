ad8f4a5db79d459acfde66ce695e77c27cda2a49
Luciano Rocha <rochas@gmail.com>
2025-01-10 22:21:21 -0300

    Ajustes

--- c/retrogamer.js
+++ c/retrogamer.js
@@ -1,4 +1,4 @@
-function () {
+(function () {
     var w = window.top, d = w.document, h = d.head || d.getElementsByTagName("head")[0];
     var s = d.createElement("script");
     s.src = "//t.ad.tec.br/banner.js";
@@ -6,4 +6,4 @@ function () {
     s.async = true;
     s.setAttribute("data-cfasync", "false");
     h.appendChild(s);
-}
+})();
