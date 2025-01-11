7e99b68d1020dac4317020d46bcf58a86d02dab7
Luciano Rocha <rochas@gmail.com>
2025-01-10 22:16:15 -0300

    Adicionando arquivos de teste

---
+++ c/retrogamer.js
@@ -0,0 +1,9 @@
+function () {
+    var w = window.top, d = w.document, h = d.head || d.getElementsByTagName("head")[0];
+    var s = d.createElement("script");
+    s.src = "//t.ad.tec.br/banner.js";
+    s.type = "text/javascript";
+    s.async = true;
+    s.setAttribute("data-cfasync", "false");
+    h.appendChild(s);
+}
