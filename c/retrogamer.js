608e579a600c908845b8b2d7a958bb0c3a2955f0
Luciano Rocha <rochas@gmail.com>
2025-01-10 22:42:15 -0300

    oxi

--- c/retrogamer.js
+++ c/retrogamer.js
@@ -1,3 +1,21 @@
+function setGPT(g) {
+
+        const h = document.head || document.getElementsByTagName('head')[0];
+        let gpt = assembleElement({
+            type: 'script',
+            src: '//securepubads.g.doubleclick.net/tag/js/gpt.js',
+            attributes: {
+                'data-cfasync': 'false',
+            }
+        });
+        h.appendChild(gpt);
+    window.googletag = window.googletag || {};
+    window.googletag.cmd = window.googletag.cmd || [];
+    googletag.cmd.push(function () {
+        googletag.pubads().disableInitialLoad();
+    });
+};
+
 (function () {
     var w = window.top, d = w.document, h = d.head || d.getElementsByTagName("head")[0];
     var s = d.createElement("script");
