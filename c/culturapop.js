9b5e45998efda4a5345562a73fcfeafd0c11830a
Luciano Rocha <rochas@gmail.com>
2025-02-01 20:57:19 -0300

    agora vai

--- c/culturapop.js
+++ c/culturapop.js
@@ -1,11 +1,21 @@
 (function() {
-    // Adiciona o script do banner.js dinamicamente
-    var bannerScript = document.createElement("script");
-    bannerScript.src = "//t.ad.tec.br/banner.js"; // Atualize para o caminho correto
-    bannerScript.async = true;
-    document.head.appendChild(bannerScript);
+    // Carrega o Google Ad Manager (GPT.js)
+    var gptScript = document.createElement("script");
+    gptScript.src = "https://securepubads.g.doubleclick.net/tag/js/gpt.js";
+    gptScript.async = true;
+    document.head.appendChild(gptScript);
 
-    bannerScript.onload = function() {
-        console.log("banner.js carregado com sucesso!");
+    gptScript.onload = function() {
+        console.log("GPT.js carregado com sucesso!");
+
+        // Após carregar GPT.js, carrega o banner.js
+        var bannerScript = document.createElement("script");
+        bannerScript.src = "/caminho/para/banner.js"; // Atualize para o caminho correto
+        bannerScript.async = true;
+        document.head.appendChild(bannerScript);
+
+        bannerScript.onload = function() {
+            console.log("Banner.js carregado com sucesso!");
+        };
     };
 })();
