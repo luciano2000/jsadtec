b457191577b0d05617450fd6371d3a1f37bdf69d
Luciano Rocha <rochas@gmail.com>
2025-02-01 21:00:39 -0300

    onloader inutil

--- c/culturapop.js
+++ c/culturapop.js
@@ -13,9 +13,5 @@
         bannerScript.src = "//t.ad.tec.br/banner.js"; // Atualize para o caminho correto
         bannerScript.async = true;
         document.head.appendChild(bannerScript);
-
-        bannerScript.onload = function() {
-            console.log("Banner.js carregado com sucesso!");
-        };
     };
 })();
