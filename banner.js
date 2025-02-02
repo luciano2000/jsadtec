9a3d13bfefe85c71dad80fac5a01a34a687ebca7
Luciano Rocha <rochas@gmail.com>
2025-02-01 21:10:53 -0300

    agora sim

--- banner.js
+++ banner.js
@@ -1,1 +1,6 @@
-console.log("Banner.js carregado com sucesso!");
+var gptScript = document.createElement("script");
+gptScript.src = "//securepubads.g.doubleclick.net/tag/js/gpt.js";
+gptScript.async = true;
+document.head.appendChild(gptScript);
+
+console.log("GPT.js e Banner.js foram carregados com sucesso!");
--- c/culturapop.js
+++ c/culturapop.js
@@ -1,17 +1,4 @@
-(function() {
-    // Carrega o Google Ad Manager (GPT.js)
-    var gptScript = document.createElement("script");
-    gptScript.src = "//securepubads.g.doubleclick.net/tag/js/gpt.js";
-    gptScript.async = true;
-    document.head.appendChild(gptScript);
-
-    gptScript.onload = function() {
-        console.log("GPT.js carregado com sucesso!");
-
-        // Após carregar GPT.js, carrega o banner.js
-        var bannerScript = document.createElement("script");
-        bannerScript.src = "//t.ad.tec.br/banner.js"; // Atualize para o caminho correto
-        bannerScript.async = true;
-        document.head.appendChild(bannerScript);
-    };
-})();
+var bannerScript = document.createElement("script");
+bannerScript.src = "//t.ad.tec.br/banner.js"; 
+bannerScript.async = true;
+document.head.appendChild(bannerScript);
