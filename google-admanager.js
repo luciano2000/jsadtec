3bfdd0c7332637a09de04341a1c236ae9d80900d
Luciano Rocha <rochas@gmail.com>
2026-05-23 19:12:28 -0300

    Add additional ad size for 'barra' viewport

--- google-admanager.js
+++ google-admanager.js
@@ -116,7 +116,7 @@
     ],
     'barra': [
       { viewport: [0, 0], sizes: [] }, // Sem anúncios em mobile
-      { viewport: [1000, 0], sizes: [{ width: 300, height: 600 }] }
+      { viewport: [1000, 0], sizes: [{ width: 300, height: 600 }, { width: 300, height: 250 }] }
     ],
     'meio': [
       { viewport: [0, 0], sizes: [{ width: 320, height: 50 }, { width: 320, height: 100 }] },
